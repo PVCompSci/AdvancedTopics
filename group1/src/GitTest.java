6003fe4f3c82805d55c4f583327c3d6cd07041e4
nlang21 <nlang21@10.11.152.134>
2019-11-01 08:35:23 -0400

    Just a test. Sorry.

--- group1/src/GitTest.java
+++ group1/src/GitTest.java
@@ -3,7 +3,6 @@ public class GitTest
 {		
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Changing things for fun");
 	}
