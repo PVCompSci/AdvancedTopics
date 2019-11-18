f9348c3865f874ecd9bae2cc80671d581fdbc077
abelgiovine20 <abelgiovine20@10.11.150.21>
2019-11-18 10:22:04 -0500

    Hi adam

--- group1/src/GitTest.java
+++ group1/src/GitTest.java
@@ -3,9 +3,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-		System.out.println("Changing things for fun");
-		System.out.println("Another change");
+		System.out.println("Hi");
 	}
 
 }
