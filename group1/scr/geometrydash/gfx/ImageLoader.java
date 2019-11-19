5715dec0ba84a8a688283f8be5c718282558483c
amoskowitz20 <amoskowitz20@192.168.1.120>
2019-11-19 08:17:45 -0500

    Fixing some issues, still cant load the world file from res folder.

--- group1/scr/geometrydash/gfx/ImageLoader.java
+++ group1/scr/geometrydash/gfx/ImageLoader.java
@@ -9,7 +9,7 @@ public class ImageLoader {
 	public static BufferedImage loadImage(String path) {
 		
 		try {
-			return ImageIO.read(new File(path));
+			return ImageIO.read(ImageLoader.class.getResource(path));
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(1);
