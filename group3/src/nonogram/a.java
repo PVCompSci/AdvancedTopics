276e997e2b61e72a9ab2beb75529507f230dec1d
ebuttikofer20 <ebuttikofer20@pascack.org>
2019-12-02 09:26:08 -0500

    i am kool

---
+++ group3/src/nonogram/a.java
@@ -0,0 +1,28 @@
+package nonogram;
+
+import java.io.File;
+
+import javax.print.attribute.standard.Media;
+
+import javafx.application.Application;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+public class a extends Application{
+
+
+	@Override
+	public void start(Stage stage) throws Exception {      
+		//push
+		String musicFile = "music.wav";
+
+//		Media sound = new Media(new File(musicFile).toURI().toString());
+//		MediaPlayer s = new MediaPlayer();
+//		s.play();
+
+	} 
+
+	public static void main(String[] args) {
+		launch(args); 
+	}
+}
