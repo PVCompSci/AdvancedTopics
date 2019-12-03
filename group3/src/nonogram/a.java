ad120d7bcd09f459c9469018d261270aa66188ee
nlang21 <nlang21@10.11.152.221>
2019-12-03 08:46:42 -0500

    Simple music player, implemented through the a class.

--- group3/src/nonogram/a.java
+++ group3/src/nonogram/a.java
@@ -1,28 +1,63 @@
 package nonogram;
 
+import javax.sound.sampled.AudioSystem;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
 import java.io.File;
 
-import javax.print.attribute.standard.Media;
+public class a
+{
+	public a()
+	{
+		//Code commented out here would allow you to choose your own music
+		/*
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(".wav Files", "wav");
 
-import javafx.application.Application;
-import javafx.scene.media.MediaPlayer;
-import javafx.stage.Stage;
+		chooser.setFileFilter(filter);
 
-public class a extends Application{
-
-
-	@Override
-	public void start(Stage stage) throws Exception {      
-		//push
-		String musicFile = "music.wav";
-
-//		Media sound = new Media(new File(musicFile).toURI().toString());
-//		MediaPlayer s = new MediaPlayer();
-//		s.play();
-
-	} 
-
-	public static void main(String[] args) {
-		launch(args); 
+		int returnVal = chooser.showOpenDialog(new JFrame());
+		
+		if(returnVal == JFileChooser.APPROVE_OPTION)
+		{
+			try
+			{
+				System.out.println(chooser.getSelectedFile());
+				File musicPath = chooser.getSelectedFile();
+				
+				AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+				Clip clip = AudioSystem.getClip();
+				clip.open(audioInput);
+				clip.start();
+				clip.loop(Clip.LOOP_CONTINUOUSLY);
+			}
+			
+			catch(Exception ex)
+			{
+				ex.printStackTrace();
+			}
+		}
+		*/
+		
+		try
+		{
+			//I don't know if this will work perfectly if exported. I think we'll need to test it somehow
+			File musicPath = new File(new File("").getAbsolutePath() + "/src/nonogram/Megalovania.wav");
+			System.out.println(musicPath);
+			
+			AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInput);
+			clip.start();
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+		}
+		
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
 	}
 }
