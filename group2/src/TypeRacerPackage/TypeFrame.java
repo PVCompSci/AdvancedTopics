a8f41a56603ce45e5067682dcc42b0bf0122e420
wjoseph21 <wjoseph21@10.11.152.156>
2019-12-03 08:28:07 -0500

    formatting

--- group2/src/TypeRacerPackage/TypeFrame.java
+++ group2/src/TypeRacerPackage/TypeFrame.java
@@ -1,53 +1,47 @@
 package TypeRacerPackage;
+
 import java.awt.*;
 import java.awt.event.*;
 
 public class TypeFrame extends Frame {
-   
-     public TypeFrame() {
-                
-        MenuBar bar = new MenuBar();
-        Menu file = new Menu();
-        MenuItem fileExit = new MenuItem();
-        
-        file.setLabel("File");
-        fileExit.setLabel("Exit");
-        
-        //action listener for menu listener
-        fileExit.addActionListener
-        (
-            new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    TypeFrame.this.windowClosed();
-                }
-            }
-        ); 
-        file.add(fileExit);
-        bar.add(file);
-        
-        setTitle("TypingGame");
-        setMenuBar(bar);
-        setSize(new Dimension(400, 400));
-        
-        this.addWindowListener
-        (
-            new WindowAdapter() {
-                public void windowClosing(WindowEvent e) {
-                    TypeFrame.this.windowClosed();
-                }
-            }
-        );  
-    }
-    
-    
-    /**
-     * Shutdown procedure when run as an application.
-     */
-    private void windowClosed() {
-    	
-    	// TODO: Check if it is safe to close the application
-    	
-        // Exit application.
-        System.exit(0);
-    }
+
+	public TypeFrame() {
+
+		MenuBar bar = new MenuBar();
+		Menu file = new Menu();
+		MenuItem fileExit = new MenuItem();
+
+		file.setLabel("File");
+		fileExit.setLabel("Exit");
+
+		// action listener for menu listener
+		fileExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TypeFrame.this.windowClosed();
+			}
+		});
+		file.add(fileExit);
+		bar.add(file);
+
+		setTitle("TypingGame");
+		setMenuBar(bar);
+		setSize(new Dimension(400, 400));
+
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				TypeFrame.this.windowClosed();
+			}
+		});
+	}
+
+	/**
+	 * Shutdown procedure when run as an application.
+	 */
+	private void windowClosed() {
+
+		// TODO: Check if it is safe to close the application
+
+		// Exit application.
+		System.exit(0);
+	}
 }
--- group2/src/TypeRacerPackage/TypeRacer.java
+++ group2/src/TypeRacerPackage/TypeRacer.java
@@ -1,4 +1,5 @@
 package TypeRacerPackage;
+
 import javafx.application.Application;
 import javafx.event.*;
 import javafx.scene.*;
@@ -18,8 +19,6 @@ import javafx.scene.text.Font;
 import javafx.scene.text.*;
 import javafx.scene.paint.*;
 
-
-
 public class TypeRacer {
 	int mode;
 	File location;
@@ -31,156 +30,129 @@ public class TypeRacer {
 	ScrollPane contentb = new ScrollPane();
 	VBox content = new VBox(2);
 	Scene gameScene;
-	
+
 	private int secondsPassed, characterCount;
-	
-	TypeRacer(int chosenOpt,File filename)
-	{
-		mode =chosenOpt;
+
+	TypeRacer(int chosenOpt, File filename) {
+		mode = chosenOpt;
 		location = filename;
 		words = new TextListener(filename);
 		getText();
 		on = 0;
 		gameStage = new Stage();
 		contenta.setAlignment(Pos.CENTER);
-		characterCount=0;
-		secondsPassed=0;
-		
-		
-		if (mode==2)
-		{
+		characterCount = 0;
+		secondsPassed = 0;
+
+		if (mode == 2) {
 			contenta.setAlignment(Pos.CENTER_LEFT);
 		}
 		content.setAlignment(Pos.TOP_CENTER);
 		content.getChildren().add(new Label("Typing Game"));
 		gameScene = new Scene(content, 600, 300);
 		gameStage.setScene(gameScene);
-		if ( mode == 0 || mode == 1 )
-		{
+		if (mode == 0 || mode == 1) {
 			content.getChildren().add(contenta);
 		}
-		if ( mode == 2 )
-		{
+		if (mode == 2) {
 			contentb.setHbarPolicy(ScrollBarPolicy.NEVER);
 			contentb.setVbarPolicy(ScrollBarPolicy.ALWAYS);
 			content.getChildren().add(contentb);
 			contentb.setContent(contenta);
 		}
-		gameScene.setOnKeyTyped(
-			new EventHandler<KeyEvent>()
-            {
-                public void handle(KeyEvent e)
-                {
-					
-                    String code = e.getCharacter().toString();
-                    checkLet(code);
-                    if(currentString==null)
-                    {
-                    	
-                    }
+		gameScene.setOnKeyTyped(new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent e) {
 
-                }
-            }); 
+				String code = e.getCharacter().toString();
+				checkLet(code);
+				if (currentString == null) {
+
+				}
+
+			}
+		});
 		gameStage.show();
-		
-		t.scheduleAtFixedRate(task, 1000, 1000);		
+
+		t.scheduleAtFixedRate(task, 1000, 1000);
 	}
-	
+
 	Timer t = new Timer();
 	TimerTask task = new TimerTask() {
-		public void run()
-		{
+		public void run() {
 			secondsPassed++;
-			System.out.println("Seconds passed: "+secondsPassed);
+			System.out.println("Seconds passed: " + secondsPassed);
 		}
 	};
-	
-	public void checkLet(String let)
-	{
+
+	public void checkLet(String let) {
 		char w = let.charAt(0);
-		if (let.charAt(0)!=(currentString.charAt(on)))
-		{
+		if (let.charAt(0) != (currentString.charAt(on))) {
 			drawWord(false);
-		}
-		else
-		{
+		} else {
 
 			on++;
 			drawWord(true);
-			if(on==currentString.length())
-			{
+			if (on == currentString.length()) {
 				getText();
 			}
 		}
 	}
-	public void drawWord(boolean correct)
-	{
+
+	public void drawWord(boolean correct) {
 		int p = 0;
 		String word = currentString;
 		contenta.getChildren().clear();
-	    for(String s : word.split( "" ))
-	    {
-	        Label l = new Label(s);
-	        if (p<on)
-	        {
-	        l.setTextFill(Color.GREEN);
-	        characterCount++;
-	        }
-	        if (p==on)
-	        {
-	        	if (correct)
-	        	{
-	        		l.setTextFill(Color.BLUE);
-	        	}
-	        	else
-	        	{
-	        		l.setTextFill(Color.RED);
-	        	}
-	        }
-	        if (p>on)
-	        {
-	        	l.setTextFill(Color.DARKGRAY);
-	        }
-	        l.setPrefWidth(20 );
-	        l.setAlignment(Pos.CENTER );
-	        l.setFont( new Font( "Arial",  24 ) );
-	        contenta.getChildren().add( l );
+		for (String s : word.split("")) {
+			Label l = new Label(s);
+			if (p < on) {
+				l.setTextFill(Color.GREEN);
+				characterCount++;
+			}
+			if (p == on) {
+				if (correct) {
+					l.setTextFill(Color.BLUE);
+				} else {
+					l.setTextFill(Color.RED);
+				}
+			}
+			if (p > on) {
+				l.setTextFill(Color.DARKGRAY);
+			}
+			l.setPrefWidth(20);
+			l.setAlignment(Pos.CENTER);
+			l.setFont(new Font("Arial", 24));
+			contenta.getChildren().add(l);
 			p++;
-	    }
-	    
+		}
+
 	}
-	public void getText() 
-	{
-		on=0;
+
+	public void getText() {
+		on = 0;
 		System.out.println("test");
-		switch(mode)
-		{
-			case 0: 
-				currentString=words.firstOpt();
+		switch (mode) {
+			case 0:
+				currentString = words.firstOpt();
 				break;
-			case 1: 
-				currentString=words
-				.secondOpt();
+			case 1:
+				currentString = words.secondOpt();
 				break;
-			
-			case 2: 
-				currentString=words.thirdOpt();
+
+			case 2:
+				currentString = words.thirdOpt();
 				break;
-			
-			default: 
+
+			default:
 				System.out.println("Error");
 				break;
 		}
-		if (currentString==null)
-		{
+		if (currentString == null) {
 			TypeMain.primaryStage.show();
 			gameStage.close();
 			t.cancel();
-			int wpm=(int)((characterCount/5)/(((double)secondsPassed)/60));
-			System.out.println("WPM: "+ wpm);
-		}
-		else
-		{
+			int wpm = (int) ((characterCount / 5) / (((double) secondsPassed) / 60));
+			System.out.println("WPM: " + wpm);
+		} else {
 			drawWord(true);
 		}
 	}
