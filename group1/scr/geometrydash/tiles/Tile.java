1bde2c60a00960b5ac0c6de24838edaed1f137b0
amoskowitz20 <amoskowitz20@192.168.1.120>
2019-12-05 11:56:45 -0500

    Added another floor spike to make things more realistic

--- group1/scr/geometrydash/tiles/Tile.java
+++ group1/scr/geometrydash/tiles/Tile.java
@@ -26,6 +26,8 @@ public class Tile {
 	public static Tile borderTile=new SolidTile(5,Assets.barrierMid);
 	public static Tile spikeTile1=new SpikeTile(3,true,new Polygon(new int[] {0,TILEWIDTH/2,TILEWIDTH},new int[] {TILEHEIGHT,0,TILEHEIGHT},3),Assets.spike1);
 	public static Tile spikeTile2=new SpikeTile(4,true,new Polygon(new int[] {0,TILEWIDTH/2,TILEWIDTH},new int[] {TILEHEIGHT,TILEHEIGHT/2,TILEHEIGHT},3),Assets.spike2);
+	public static Tile spikeTile3=new SpikeTile(27,true,new Polygon(new int[]{0,0,TILEWIDTH,TILEWIDTH},new int[] {TILEHEIGHT,3*TILEHEIGHT/4,3*TILEHEIGHT/4,TILEHEIGHT},3),Assets.spike3);
+	
 	public static Tile barrierMid=new SolidTile(5,Assets.barrierMid);
 	public static Tile barrierSL=new SolidTile(6,Assets.barrierSL);
 	public static Tile barrierST=new SolidTile(7,Assets.barrierST);
